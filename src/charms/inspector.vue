429314a34074f16225725c0d8b9db444d447f312
Seth L. Blumberg <sethb@pobox.com>
2019-10-19 17:39:23 -0400

    refactor formatDescription into separate file

--- src/charms/inspector.vue
+++ src/charms/inspector.vue
@@ -3,6 +3,8 @@ import Markdown from 'markdown-it';
 import MarkdownDeflist from 'markdown-it-deflist';
 import { mapState } from 'vuex';
 
+import { formatDescription } from '@/charms/make-md';
+
 const CHARM_ID_REGEXP = /^([^\s.]+\.[^\s.]+)(?:\.(\S+))?$/u;
 
 const markdownProcessor = new Markdown({ breaks: true })
@@ -51,74 +53,6 @@ export default {
     },
   },
 };
-
-function formatDescription (charm, variant, group) {
-  let name = charm.name;
-  if (variant?.name) {
-    if (name.includes('{')) {
-      name = name.replace(/\{.*\}/u, variant.name);
-    } else {
-      name = `${name}: ${variant.name}`;
-    }
-  }
-  let description = reformatLineBreaks(charm.description);
-  if (variant?.description) {
-    description = `${description}
-### ${variant.name}
-${reformatLineBreaks(variant.description)}`;
-  }
-  const p = charm.prerequisites;
-  const minEssence = Math.max(p?.essence || 0, variant?.prerequisites?.essence || 0);
-  const minTraits = { ...p?.traits || {}, ...variant?.prerequisites?.traits || {} };
-  const minima = [];
-  for (const [k, v] of Object.entries(minTraits)) {
-    minima.push(`${k === '_' ? group : k} ${v}`);
-  }
-  if (minEssence) {
-    minima.push(`Essence ${minEssence}`);
-  }
-  let details = [];
-  if (charm.type !== 'knack') {
-    const keywords = Object.entries(charm.keywords || {}).map(([k, v]) => {
-      if (v === true) {
-        return k;
-      }
-      return `${k} ${v}`;
-    });
-    if (charm.martial) {
-      keywords.push(`Martial (${Object.keys(charm.martial).sort().join(', ')})`);
-    }
-    if (charm['martial-ready']) {
-      keywords.push('Martial-Ready');
-    }
-    if (charm.virtue) {
-      const keys = Object.keys(charm.virtue);
-      keys.sort();
-      keywords.push(...keys.map((v) => `Virtue (${v})`));
-    }
-    keywords.sort();
-    details = [
-      `\n**Keywords:** ${keywords.length ? keywords.join(', ') : '\u2014'}`,
-      `\n**Cost:** ${charm.cost || '\u2014'}`,
-      `\n**Action:** ${charm.action}`,
-    ];
-    if (charm.duration) {
-      details.push(`\n**Duration:** ${charm.duration}`);
-    }
-  }
-  return `## ${name}
-**Minima:** ${minima.length ? minima.join(', ') : '(see below)'}${details.join('')}
-
-${description}`;
-}
-
-function reformatLineBreaks (description) {
-  description = (description || '')
-    .replace(/\n([^-|])/gu, '\n\n$1')
-    .replace(/(\n- [^\n]+)\n\n([^-])/gu, '$1\n$2')
-    .replace(/\n+$/u, '');
-  return description;
-}
 </script>
 
 <template>
